25eef219e08e3aab3c8bfd0eb0aca774f908f877
dparalen <vetrisko@gmail.com>
2014-08-20 09:49:24 +0200

    warn about the infamous KeyError exception due to monkey patching of threading module

--- dva/__init__.py
+++ dva/__init__.py
@@ -1,3 +1,6 @@
-from gevent import monkey; monkey.patch_all(thread=False)
+import sys
+if 'threading' in sys.modules:
+    print >>sys.stderr, '# Warning: expect an Un-handled KeyError at the exit of the program --- threading was imported before gevent!'
+from gevent import monkey; monkey.patch_all()
 from gevent import socket
 socket.setdefaulttimeout(3)
