138c43d84763c2c41c97e35a5cf383a50a1dde84
dparalen <vetrisko@gmail.com>
2014-08-21 22:26:45 +0200

    introducing bugzilla result reporting

--- dva/report/result.py
+++ dva/report/result.py
@@ -41,29 +41,37 @@ def get_stage_result(stage_data, verbose=False):
                                     subsequent_indent='  '))
     return ret, log
 
-def get_hwp_result(hwp_data, verbose=False):
+
+def get_hwp_result(data, verbose=False):
     '''get overal hwp result'''
     ret = RESULT_PASSED
     log = []
-    for hwp in hwp_data:
-        hwp_status = RESULT_PASSED
+    for res in data:
+        if 'test' in res:
+            # test case result
+            sub_result, sub_log = get_test_result(res['test'], verbose)
+        else:
+            # stage result
+            sub_result, sub_log = get_stage_result(res, verbose)
+        if sub_result in [RESULT_ERROR, RESULT_FAILED] and ret == RESULT_PASSED:
+            ret = sub_result
+        log.extend(sub_log)
+    return ret, log
+
+def get_ami_result(data, verbose=False):
+    '''get overal ami result'''
+    ret = RESULT_PASSED
+    log = []
+    for hwp in data:
         hwp_index = len(log)
-        for res in hwp_data[hwp]:
-            if 'test' in res:
-                # test case result
-                sub_result, sub_log = get_test_result(res['test'], verbose)
-            else:
-                # stage result
-                sub_result, sub_log = get_stage_result(res, verbose)
-            if sub_result in [RESULT_ERROR, RESULT_FAILED] and ret == RESULT_PASSED:
-                hwp_status = sub_result
-            log.extend(sub_log)
-        hwp_header = '%s: %s' % (hwp, ret)
-        log.insert(hwp_index, '-' * len(hwp_header))
-        log.insert(hwp_index, hwp_header)
+        sub_result, sub_log = get_hwp_result(data[hwp], verbose)
+        if sub_result != RESULT_PASSED and ret == RESULT_PASSED:
+           ret = sub_result
+        header = '%s: %s' % (hwp, sub_result)
+        log.insert(hwp_index, '-' * len(header))
+        log.insert(hwp_index, header)
         log.insert(hwp_index, '')
-        if hwp_status != RESULT_PASSED and ret == RESULT_PASSED:
-           ret = hwp_status
+        log.extend(sub_log)
     return ret, log
 
 def get_overall_result(data, verbose=False):
@@ -78,7 +86,7 @@ def get_overall_result(data, verbose=False):
         for arch in agg_data[region]:
             for itype in agg_data[region][arch]:
                 for ami in agg_data[region][arch][itype]:
-                    sub_result, sub_log = get_hwp_result(agg_data[region][arch][itype][ami], verbose)
+                    sub_result, sub_log = get_ami_result(agg_data[region][arch][itype][ami], verbose)
                     if sub_result != RESULT_PASSED and ret == RESULT_PASSED:
                         ret = sub_result
                     ami_header = '%s %s %s %s: %s' % (region, arch, itype, ami, ret)
