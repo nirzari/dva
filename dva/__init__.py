e223399fb8b1467b9b6215343eb0ecae713a4aad
dparalen <vetrisko@gmail.com>
2014-08-21 22:21:50 +0200

    solved the gevent-threading import race

--- dva/__init__.py
+++ dva/__init__.py
@@ -1,6 +1,4 @@
 import sys
-if 'threading' in sys.modules:
-    print >>sys.stderr, '# Warning: expect an Un-handled KeyError at the exit of the program --- threading was imported before gevent!'
 from gevent import monkey; monkey.patch_all()
 from gevent import socket
 socket.setdefaulttimeout(3)
