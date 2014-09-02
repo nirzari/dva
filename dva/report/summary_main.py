0cbb842b683c12d0192b7ac54c4c292793d9cba6
Martin Minar <mminar@redhat.com>
2014-09-02 13:14:55 +0200

    Summary module base.

---
+++ dva/report/summary_main.py
@@ -0,0 +1,22 @@
+'''
+validation summary report function module
+'''
+import sys
+import time
+import logging
+import bugzilla
+import tempfile
+import aggregate
+from ..tools.retrying import retrying, EAgain
+from ..work.data import load_yaml, save_result
+from ..work.common import RESULT_PASSED
+from result import get_hwp_result
+from gevent.pool import Pool
+from gevent.coros import RLock
+import yaml
+try:
+    from yaml import CDumper as Dumper
+except ImportError:
+    from yaml import Dumper
+
+logger = logging.getLogger(__name__)
