72666d8a15b29d9cc0dae11eab3bf071f5e6c062
dparalen <vetrisko@gmail.com>
2014-09-25 10:04:38 +0200

    fix: missing dep

--- dva/test/testcase_24_yum_plugin.py
+++ dva/test/testcase_24_yum_plugin.py
@@ -8,6 +8,7 @@ class testcase_24_yum_plugin(Testcase):
     """
     stages = ['stage1']
     applicable = {'product': '(?i)RHEL|BETA', 'version': 'OS (>=5.5, <7.0)'}
+    after = ['testcase_11_package_set']
     tags = ['default']
 
     # pylint: disable=W0613
