822799ccc3cbcdc9e2d4d679fbdea3851de17b59
Irina Gulina <igulina@redhat.com>
2014-10-13 21:24:34 +0200

    issue #18 - introducing tab completion

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ from setuptools import setup
 import glob
 
 setup(name='dva',
-    version='0.4',
+    version='0.1',
     description="dva's validation. again.",
     author='dparalen',
     author_email='vetrisko@gmail.com',
     url='https://github.com/RedHatQE/dva',
     license="GPLv3+",
     provides='dva',
-    install_requires=['PyYAML', 'aaargh', 'stitches>=0.7', 'boto', 'gevent','html'],
+    install_requires=['PyYAML', 'aaargh', 'stitches>=0.7', 'boto'],
     packages=[
         'dva', 'dva.test', 'dva.cloud', 'dva.work', 'dva.tools', 'dva.connection', 'dva.report'
         ],
@@ -19,7 +19,8 @@ setup(name='dva',
              ('/usr/share/dva/hwp', glob.glob('hwp/*.yaml')),
              ('/usr/share/dva/data', glob.glob('data/*')),
              ('/usr/share/dva/examples', glob.glob('examples/*.yaml')),
-             ('/etc', ['etc/dva.yaml'])
+             ('/etc', ['etc/dva.yaml']),
+             ('/etc/bash_completion.d/', ['etc/bash_completion.d/dva'])
     ],
     classifiers=[
             'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
