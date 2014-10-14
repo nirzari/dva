1f47d77e0e63512b89ef4d65f205edb43b7ebf9c
Martin Minar <mminar@redhat.com>
2014-10-14 12:29:20 +0200

    Fixing https://github.com/RedHatQE/dva/issues/27 - summary now properly reports skipped instance types.

--- dva/report/summary_main.py
+++ dva/report/summary_main.py
@@ -7,7 +7,7 @@ import logging
 import bugzilla
 import tempfile
 import aggregate
-import html #in order for this to work you need to do "pip install html"
+from html import HTML
 from ..tools.retrying import retrying, EAgain
 from ..work.data import load_yaml, save_result
 from ..work.common import RESULT_PASSED
@@ -32,17 +32,29 @@ def print_failed(data, aname, area, whitelist,area2='cloudhwname'):
                     if test['test']['name'] not in whitelist:
                         print('   Failed test %s (%s)' % (test['test']['name'],test[area2]))
             else:
-                if test['stage_result'] != 'passed':
+                if test['stage_result'] == 'skip':
+                    print('!! Skipped - most likely unsupported instance type in region. (%s)' % test[area2])
+                elif test['stage_result'] == 'failed':
                     print('!! Failed stage %s (%s)' % (test['stage_name'],test[area2]))
 
 def print_failed_html(data, aname, area, whitelist,area2='cloudhwname'):
     agg_data = aggregate.flat(data, area)
-    table_data=[]
+    h = HTML('html')
+    t = h.table(border='1')
     for name,data in agg_data.items():
+        r = t.tr
+        r.td(name[0])
         for test in data:
-            pass
-    html_output = html.table(table_data)
-    print html_output
+            if test.has_key('test'):
+                if test['test']['name'] not in whitelist:
+                    testname=test['test']['name']
+                    if test['test']['result'] != 'passed':
+                        color='red'
+                    else:
+                        color='green'
+                        testname='OK'
+                    r.td(testname,bgcolor=color)
+    print t
 
 def main(config, istream,test_whitelist,compare,html):
     logger.debug('starting generation from file %s',istream)
