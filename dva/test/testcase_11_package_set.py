b9c1818076ccff8504e7d106e8a0cae18c7f6102
dparalen <vetrisko@gmail.com>
2015-01-19 16:08:31 +0100

    introducing yaml-based, product--platform-indexed package-list files

--- dva/test/testcase_11_package_set.py
+++ dva/test/testcase_11_package_set.py
@@ -1,7 +1,9 @@
 """ This module contains testcase_11_package_set test """
 import os
 import re
-from testcase import Testcase
+from testcase import Testcase, SkipException
+from dva.work.data import load_yaml
+FILENAME_FIELDS = ['platform', 'product', 'version']
 
 
 class testcase_11_package_set(Testcase):
@@ -13,42 +15,47 @@ class testcase_11_package_set(Testcase):
     applicable = {'platform': '(?i)RHEL|BETA|FEDORA'}
     tags = ['default']
 
+    def package_list_filename(self, params):
+        '''construct package list filename based on the params provided'''
+        filename = 'packages_' + '_'.join([params[field].lower() for field in FILENAME_FIELDS]) + '.yaml'
+        # beta packages are the same as rhel
+        filename = filename.replace('beta', 'rhel')
+        path = os.path.sep.join([self.datadir, filename])
+        if not os.path.exists(path):
+            raise SkipException('packages list %s not found' % path)
+        return path
+
+    def required_packages(self, params):
+        '''return set of required packages'''
+        with open(self.package_list_filename(params)) as fd:
+            ret = set(load_yaml(fd))
+        return ret
+        
+
     def test(self, connection, params):
         """ Perform test """
+        # load expected package list
+        package_set_required = self.required_packages(params)
 
-        packages = self.match(connection,
+        # fetch packages installed on the instance
+        packages_log = self.match(connection,
                               'rpm -qa --queryformat \'%{NAME},\' && echo',
                               re.compile('.*\r\n(.*),\r\n.*', re.DOTALL),
-                              timeout=30)
-        if packages:
-            basepath = self.datadir + '/packages_'
-            path = ''
-            if params['platform'].upper() == 'FEDORA':
-                basepath += 'fedora_'
-                if os.path.exists(basepath + params['version']):
-                    path = basepath + params['version']
-            else:
-                basepath += 'rhel_'
-                if (len(params['version']) > 2) and os.path.exists(basepath + params['version'][0] + params['version'][2:]):
-                    path = basepath + params['version'][0] + params['version'][2:]
-                elif (len(params['version']) > 0) and os.path.exists(basepath + params['version'][0]):
-                    path = basepath + params['version'][0]
-            if path == '':
-                self.log.append({'result': 'skip', 'comment': 'no package set for this os version'})
-                return self.log
-            with open(path, 'r') as psetfile:
-                package_set_requred = set(psetfile.read().split('\n')[:-1])
-            package_set_got = set(packages[0].split(','))
-            if params['userdata'].find('yum -y install xdelta') != -1:
-                # package was installed by cloud-init
-                package_set_got.discard('xdelta')
-            difference = package_set_requred.difference(package_set_got)
-            difference_new = package_set_got.difference(package_set_requred)
-            self.log.append({'result': 'passed', 'comment': 'Newly introduced packages: ' + str(sorted(list(difference_new)))})
-            if params['platform'].upper() == 'BETA' and len(difference) > 1:
-                self.log.append({'result': 'failed', 'comment': 'Beta may lack not more than 1 package: ' + str(sorted(list(difference)))})
-            elif len(difference) > 1:
-                self.log.append({'result': 'failed', 'comment': 'some packages are missing: ' + str(sorted(list(difference)))})
-            else:
-                self.log.append({'result': 'passed', 'comment': 'All required package are included'})
+                              timeout=30)[0]
+        package_set_got = set(packages_log.split(','))
+
+        # augment userdata-installed packages
+        if params['userdata'].find('yum -y install xdelta') != -1:
+            package_set_got.discard('xdelta')
+
+        difference = package_set_required.difference(package_set_got)
+        difference_new = package_set_got.difference(package_set_required)
+        
+        self.log.append({'result': 'passed', 'comment': 'Newly introduced packages: ' + str(sorted(list(difference_new)))})
+        if params['platform'].upper() == 'BETA' and len(difference) > 1:
+            self.log.append({'result': 'failed', 'comment': 'Beta may lack not more than 1 package: ' + str(sorted(list(difference)))})
+        elif len(difference) > 1:
+            self.log.append({'result': 'failed', 'comment': 'some packages are missing: ' + str(sorted(list(difference)))})
+        else:
+            self.log.append({'result': 'passed', 'comment': 'All required package are included'})
         return self.log
