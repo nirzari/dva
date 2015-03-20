09905cab55a5c6e7c9fafdd84cc4a15b9578ea0a
Martin Minar <mminar@redhat.com>
2015-03-20 14:09:16 +0100

    First version of SRioV test - need to add limitation for instance types.

---
+++ dva/test/testcase_63_sriov.py
@@ -0,0 +1,18 @@
+""" This module contains testcase_63_sriov test """
+from testcase import Testcase
+
+
+class testcase_63_sriov(Testcase):
+    """
+    Check various cpu flags
+    """
+    stages = ['stage1', 'stage2']
+    tags = ['default', 'kernel']
+    applicable = {'virtualization': 'hvm', 'platform': '(?i)BETA|RHEL|ATOMIC', 'version': 'OS (>=6.6)'}
+
+    # pylint: disable=unused-argument
+    def test(self, connection, params):
+        """ Perform test """
+        self.get_return_value(connection, 'lsmod | grep ixgbevf')
+
+        return self.log
