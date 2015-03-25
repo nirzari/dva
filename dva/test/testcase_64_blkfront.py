48890a42b3431230b78ad31c7b8a903bd5100952
Martin Minar <mminar@redhat.com>
2015-03-25 11:19:19 +0100

    Adding test for bugzilla Bug 1202393 - RFE: support for indirect descriptors in RHEL 6

---
+++ dva/test/testcase_64_blkfront.py
@@ -0,0 +1,19 @@
+""" This module contains testcase_64_blkfront test """
+from testcase import Testcase
+
+
+class testcase_64_blkfront(Testcase):
+    """
+    Check various cpu flags
+    """
+    stages = ['stage1', 'stage2']
+    tags = ['default', 'kernel']
+    applicable = {'virtualization': 'hvm', 'platform': '(?i)BETA|RHEL|ATOMIC', 'version': 'OS (>=6.7)', 'cloudhwname': 'i2.*'}
+
+    # pylint: disable=unused-argument
+    def test(self, connection, params):
+        """ Perform test of bug 1202393"""
+        self.get_return_value(connection, 'dmesg | grep blkfront')
+        self.get_return_value(connection, 'dmesg | grep blkfront | grep "persistent grants: enabled; indirect descriptors: disabled"')
+
+        return self.log
