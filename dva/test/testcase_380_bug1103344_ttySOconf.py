3921473197eea3ec5d1c1882291fd25404bc10eb
Martin Minar <mminar@redhat.com>
2015-03-26 13:13:37 +0100

    Bug should not be present in RHEL7.

--- dva/test/testcase_380_bug1103344_ttySOconf.py
+++ dva/test/testcase_380_bug1103344_ttySOconf.py
@@ -8,7 +8,7 @@ class testcase_380_bug1103344_ttySOconf(Testcase):
     """
     stages = ['stage1', 'stage2']
     tags = ['default', 'kernel']
-    applicable = {'virtualization': 'hvm', 'platform': '(?i)BETA|RHEL|ATOMIC', 'version': 'OS (>=6.7)'}
+    applicable = {'virtualization': 'hvm', 'platform': '(?i)BETA|RHEL|ATOMIC', 'version': 'OS (>=6.7, <7.0)'}
 
     # pylint: disable=unused-argument
     def test(self, connection, params):
