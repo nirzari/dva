f64a5c9b168c790b62d945acb642e20afa16ea32
Martin Minar <mminar@redhat.com>
2015-06-16 14:43:20 +0200

    Rewrite of __init__.py to dynamically load testcases in directory.

--- dva/test/__init__.py
+++ dva/test/__init__.py
@@ -1,65 +1,9 @@
-""" All testing modules """
+import os
+for module in os.listdir(os.path.dirname(__file__)):
+    if module == '__init__.py' or module == 'testcase.py' or module[-3:] != '.py':
+        continue
+    # Dynamically import all testcases in directory.
+    __import__(module[:-3], locals(), globals())
+del module
+del os
 
-import testcase_01_bash_history
-import testcase_02_selinux_context
-import testcase_03_running_services
-import testcase_04_cloud_firstboot
-import testcase_05_grub
-import testcase_06_inittab
-import testcase_07_libc6_xen_conf
-import testcase_08_memory
-import testcase_09_nameserver
-import testcase_10_networking
-import testcase_11_package_set
-import testcase_12_passwd_group
-import testcase_13_resize2fs
-import testcase_14_host_details
-import testcase_141_hostname
-import testcase_15_rhel_version
-import testcase_16_selinux
-import testcase_17_shells
-import testcase_18_sshd
-import testcase_19_rhn_system_id
-import testcase_20_auditd
-import testcase_21_disk_size_format
-import testcase_22_gpg_keys
-import testcase_23_syslog
-import testcase_24_yum_plugin
-import testcase_25_uname
-import testcase_26_verify_rpms
-import testcase_27_yum_repos
-import testcase_28_iptables
-import testcase_29_swap_file
-import testcase_30_rhn_certificates
-import testcase_31_subscription_management
-import testcase_32_ephemeral
-import testcase_33_userdata
-import testcase_34_cpu
-import testcase_35_console
-import testcase_360_ebs
-import testcase_361_ebs_deferred_detach
-import testcase_37_sshd_bug923996
-import testcase_38_rclocal_checksum
-import testcase_380_bug1103344_ttySOconf
-import testcase_39_root_is_locked
-import testcase_41_rh_amazon_rhui_client
-import testcase_42_ipv6
-import testcase_43_yum_versionlock
-import testcase_50_yum_package_install
-import testcase_55_yum_group_install
-import testcase_60_yum_update
-import testcase_61_yum_proxy
-import testcase_62_cpuflags
-import testcase_63_sriov
-import testcase_64_blkfront
-import testcase_65_numa
-import testcase_66_bug1122300_proc_cmdline
-import testcase_80_no_avc_denials
-import testcase_90_1_kernel_bug874053
-import testcase_90_2_cgroups_kernel_bug914737
-import testcase_90_3_bug1074660
-import testcase_97_collect_packagelist
-import testcase_98_dmesg_stage1
-import testcase_98_kernel_upgrade_pre
-import testcase_98_package_install_pre
-import testcase_98_kernel_parameter
