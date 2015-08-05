fb439b162736ed302b6ece6017be2b72085e29eb
Irina Gulina <igulina@redhat.com>
2015-08-05 14:51:21 +0200

    Fix issue #52 - Summary report for termination

--- dva/work/terminate.py
+++ dva/work/terminate.py
@@ -3,10 +3,11 @@ terminate all created instances
 '''
 import re
 import sys
+import os
 from data import record_cloud_config
 from stage import terminate_instance
 from boto.exception import EC2ResponseError
-
+from dva.cloud.base import PermanentCloudException
 
 ID_PARAMS = {
     'cloud': None,
@@ -35,6 +36,12 @@ def main(conf, istream, ostream, cloud, verbose):
 
     id_region_set = set(id_region_list)
     unique_id_region_list=list(id_region_set)
+    print '=' * 72
+    print 'Requested instances to terminate: {:>6}'.format(len(unique_id_region_list))
+    print '=' * 72
+    terminated_instances=0
+    not_terminated_instances=[]
+    not_found_instances=0
     for id_region in unique_id_region_list:
         instance_id_region = id_region[11:].split('_')
 	p = ID_PARAMS.copy()
@@ -42,14 +49,29 @@ def main(conf, istream, ostream, cloud, verbose):
         p['region'] = instance_id_region[1]
         p['cloud'] = cloud
         record_cloud_config(p, conf)
-        print p['id'] + " in a region " + p['region'] + " will be terminated."
         try:
             terminate_instance(p)
-            print p['id'], p['region'], "was terminated"
+            print 'Ami {0} in {1:>15} region was terminated.'.format(p['id'], p['region'])
+            terminated_instances += 1
         except KeyboardInterrupt:
             sys.exit()
         except EC2ResponseError:
             print "The instance {0} in {1} region may not be terminated. Modify its 'disableApiTermination' instance attribute and try again.".format(p['id'], p['region'])
+            not_terminated_instances.append((p['id'], p['region']))
+        except PermanentCloudException:
+            print 'Ami {0} in {1:>15} region was NOT FOUND.'.format(p['id'], p['region'])
+            not_found_instances += 1
         except Exception as e:
-            print "Unexpected error: {0}".format(e)
+            print 'Unexpected error: {0}'.format(e)
             raise
+    print '\nTermination Summary'
+    print '=' * 72
+    print 'Requested instances to terminate: {:>5}'.format(len(unique_id_region_list))
+    print 'Terminated: {:>27}'.format(terminated_instances)
+    print 'Not found: {:>28}'.format(not_found_instances)
+    print 'Not terminated: {:>23}'.format(len(not_terminated_instances))
+    if not_terminated_instances:
+        for instance in not_terminated_instances:
+            print 'Ami {0} in {1:>12} region was NOT TERMINATED.'.format(instance[0], instance[1])
+    else:
+        print "\nAll found instances were terminated.\n"
