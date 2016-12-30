76c06a6dd28eef9c042c408652560c98bb6d2d3d
Vratislav Hutsky <vhutsky@redhat.com>
2016-12-30 21:37:50 +0100

    Update of auditd test for RHEL 7.3

--- dva/test/testcase_20_auditd.py
+++ dva/test/testcase_20_auditd.py
@@ -43,8 +43,12 @@ class testcase_20_auditd(Testcase):
             self.ping_pong(connection, 'md5sum /etc/sysconfig/auditd  | cut -f 1 -d \' \'', auditd_sysconf_checksum)
         elif version < '8.0':
             # RHEL7.x
-            auditd_checksum = 'e1886162554c18906df2ecd258aa4794'
-            auditd_rules_checksum = 'd5985c09d6c150e433362eca9d59e8fe'
+            if version <= '7.2':
+                auditd_checksum = 'e1886162554c18906df2ecd258aa4794'
+                auditd_rules_checksum = 'd5985c09d6c150e433362eca9d59e8fe'
+            else:
+                auditd_checksum = '643fcb75e59e330539c91678a2bb6454'
+                auditd_rules_checksum = '6b6d7a773a63a2cef32341993ad66c8c'
         self.ping_pong(connection, 'md5sum /etc/audit/auditd.conf | cut -f 1 -d \' \'', auditd_checksum)
         self.ping_pong(connection, 'md5sum /etc/audit/audit.rules | cut -f 1 -d \' \'', auditd_rules_checksum)
         return self.log
